bbcb553f0f4636dd40e84d320a576b3de7b95357
Arik Nemtsov <arik@wizery.com>
2010-11-11 18:10:26 +0200

    compiles for froyo with Android.mk
    renamed include/linux/if.h header to not interfere with definitions.
    added missing definitions for use by hostapd.

--- include/netlink-local.h
+++ include/netlink-local.h
@@ -337,7 +337,6 @@ static inline int nl_cb_call(struct nl_cb *cb, int type, struct nl_msg *msg)
 }
 
 #define ARRAY_SIZE(X) (sizeof(X) / sizeof((X)[0]))
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 
 #define __init __attribute__ ((constructor))
 #define __exit __attribute__ ((destructor))
